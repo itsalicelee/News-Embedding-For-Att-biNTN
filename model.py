2d90b9c2f94c5c8241f2b2cc78906651aa5d806c
itsalicelee <alice668149@gmail.com>
2021-07-12 15:23:21 +0800

    update model with batch size=128

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import random
 import numpy as np
-
+import math 
 # Read file
 # ttv consists of [[tickerList1, dateStr1, titleStr1, triple1, vector1] ... ]
 # For training, we only need vector1, and its shape is 3*100
@@ -17,6 +17,24 @@ with open('./results/ttv.pkl', 'rb') as handle:
 train_set = []
 for i in range(len(f)):
     train_set.append(f[i][4]) # vector1 is the 5th element
+
+
+def getnumpy(train_set):
+    e1, re, e2, corrupted = [], [], [], []
+    for triple in train_set:
+        e1.append(triple[0])
+        re.append(triple[1])
+        e2.append(triple[2])
+        corrupted.append(choose_any_word(train_set))
+    e1 = np.array(e1)
+    re = np.array(re)
+    e2 = np.array(e2)
+    corrupted = np.array(corrupted)
+    #print("getTensor shape:", e1.shape, re.shape, e2.shape, corrupted.shape)
+    return e1, re, e2, corrupted
+    
+        
+
 
 # set up random seed and device
 seed = 2021
@@ -33,8 +51,25 @@ config = {
     'weight_decay': 1e-5, 
     'embedding_size' : 100,
     'tensor_dim' : 100,
-    'n_epochs': 200,                # maximum number of epochs
+    'batch_size': 128,
+    'n_epochs': 200, 
 }
+
+# self-defined dataset
+class NewsDataset(Dataset):
+    def __init__(self, e1, re, e2, corrupted):
+        self.e1 = e1
+        self.re = re
+        self.e2 = e2
+        self.corrupted = corrupted
+
+    def __getitem__(self, idx):
+        return self.e1[idx], self.re[idx], self.e2[idx], self.corrupted[idx]
+    
+    def __len__(self):
+        return len(train_set)
+
+
 
 # A sub-model of NTN
 class SubNTN(nn.Module):
@@ -46,7 +81,7 @@ class SubNTN(nn.Module):
         self.linear = nn.Linear(embedding_size * 2, tensor_dim)
         self.relu = nn.ReLU()
     def forward(self, e1, re):
-        e1_re = torch.cat((e1, re))
+        e1_re = torch.cat((e1, re), dim = 1)
         S1 = self.relu(self.bilinear(e1,re) + self.linear(e1_re))
         return S1
 
@@ -85,71 +120,77 @@ def choose_any_word(train_set):
     out = out[rand]
     return out
         
-def train(model, config, device):
+def train(model, config, device, train_dataloader):
     n_epochs = config['n_epochs']
-    optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'], weight_decay =config['weight_decay'])
+    batch_size = config['batch_size']
+    best_loss = 999
     for epoch in trange(0, n_epochs):
-        model.train()                           
-        losses = []
-        for i in range(len(train_set)):  # triple shape: (3, 100)
-            loss = 999
-            cnt  = 0
-            while loss > 1e-5:
-                cnt += 1 
-                triple = train_set[i]
-                e1, re, e2 = triple[0], triple[1], triple[2]
-                optimizer.zero_grad()        
-                e1 = torch.from_numpy(e1).to(device)
-                re = torch.from_numpy(re).to(device)
-                e2 = torch.from_numpy(e2).to(device)
-                corrupted = choose_any_word(train_set)
-                corrupted = torch.from_numpy(corrupted).to(device)
-                G, C, Cinv = model(e1, re, e2) 
-                rand = random.randint(1,2)
-                if rand % 2 == 0:  # corrupt e1
-                    Gc, Cc, Ccinv = model(corrupted, re, e2)    
-                else:  # corrupt e2
-                    Gc, Cc, Ccinv = model(e1, re, corrupted)
-                loss = torch.clamp(1 - G + Gc, min=0, max=None)
-                loss.backward()
-                optimizer.step() 
-                if cnt > 10000:
-                    print(i, "is not finished!:(")
-                    break
+        model.train()
+        for i, (e1, re, e2, corrupted) in enumerate(train_dataloader):  # triple shape: (3, 100)
+            optimizer.zero_grad()        
+            e1 = e1.to(device)
+            re = re.to(device)
+            e2 = e2.to(device)
+            corrupted = corrupted.to(device)
+            G, C, Cinv = model(e1, re, e2) 
+            rand = random.randint(1,2)
+            if rand % 2 == 0:  # corrupt e1
+                Gc, Cc, Ccinv = model(corrupted, re, e2)    
+            else:  # corrupt e2
+                Gc, Cc, Ccinv = model(e1, re, corrupted)
+            loss = torch.clamp(1 - G + Gc, min=0, max=None)
+            loss = torch.flatten(loss, start_dim=0)
+            loss = torch.sum(loss)/len(loss)
+            loss.backward()
+            optimizer.step() 
            
-            if i % 100 == 0:
-                print("Epoch: {:03d} ==> Progress: {:05d}/{:05d}".format(epoch, i, len(train_set)))
-                torch.save(model.state_dict(), './results/model.ckpt')
+            if (i+1) % 20 == 0:
+                print('[{:03d}/{:03d}] | Step:{:04d}/{:04d} | Train Loss: {:.4f}'.format
+                        (epoch + 1, n_epochs, i+1, math.ceil(len(train_set)/batch_size), loss))
+            if loss < best_loss: 
+                best_loss = loss
+                torch.save(model.state_dict(), './results/best_model.ckpt')
+            else:
+                torch.save(model.state_dict(), './results/latest_model.ckpt')
             
-
 def validation(model):
     # given the trained model, return the record
     # record will be consist of many numpy arrays, each array is the concatenated result of C and Cinv
     record = []
-    for i in range(len(train_set)):
-        triple = train_set[i]
-        e1, re, e2 = triple[0], triple[1], triple[2]
-        optimizer.zero_grad() 
-        e1 = torch.from_numpy(e1).to(device)
-        re = torch.from_numpy(re).to(device)
-        e2 = torch.from_numpy(e2).to(device)
-        corrupted = choose_any_word(train_set)
-        corrupted = torch.from_numpy(corrupted).to(device)
-        rand = random.randint(1,2)
+    cnt = 0
+    print("Start testing...")
+    for i, (e1, re, e2, corrupted) in enumerate(test_dataloader):  # triple shape: (3, 100)
+        optimizer.zero_grad()
+        e1 = e1.to(device)
+        re = re.to(device)
+        e2 = e2.to(device)
+        corrupted = corrupted.to(device)
         G, C, Cinv = model(e1, re, e2)
-        if rand % 2 == 0: # corrupt e1
+        rand = random.randint(1,2)
+        if rand % 2 == 0:  # corrupt e1
             Gc, Cc, Ccinv = model(corrupted, re, e2)
         else:  # corrupt e2
             Gc, Cc, Ccinv = model(e1, re, corrupted)
-        result = torch.cat((C,Cinv))
-        record.append(result.cpu().detach().numpy()) # Save concated result of C and Cinv to record
+        result = torch.cat((C, Cinv))
+        record.append(result.cpu().detach().numpy()) # Save the result of concatenated C and Cinv to record
+        cnt += 1
+        
+        if (cnt+1) % 50 == 0:
+            print(str(cnt+1) + " titles are done!")
+ 
     return record
 
 
-'''load pickle'''
-undone = []
+
+'''main'''
+e1, re, e2, corrupted = getnumpy(train_set) # get e1, re, e2 corrupted as np.array
+dataset = NewsDataset(e1, re, e2, corrupted) # load to dataset
+train_dataloader = DataLoader(dataset, batch_size=config['batch_size'], shuffle=True)
 model = NTN(embedding_size=config['embedding_size'], tensor_dim=config['tensor_dim']).to(torch.double).to(device)
-train(model, config, device)
+# model.load_state_dict(torch.load('./results/best_model.ckpt'))
+optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'], weight_decay =config['weight_decay'])
+train(model, config, device, train_dataloader)
+test_dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
 record = validation(model)
 with open('./results/record.pkl', 'wb') as f:
     pickle.dump(record, f)
